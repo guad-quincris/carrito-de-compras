2037692afb8c9062f871380b3af5a6695f23d295
guad-quincris <guad.quincris@hotmail.com>
2021-04-25 19:49:38 -0500

    implementacion del formulario para singin

--- src/app/auth/components/sigin/sigin.component.ts
+++ src/app/auth/components/sigin/sigin.component.ts
@@ -10,17 +10,35 @@ import { AuthService } from 'src/app/core/services/auth.service';
 })
 export class SiginComponent implements OnInit {
 
-form: FormGroup;
+  form: FormGroup;
 
   constructor(
-    private formBulder: FormBuilder,
+    private formBuilder: FormBuilder,
     private router: Router,
     private authService: AuthService
   ) {
-
+    this.buildForm();
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  register(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const value = this.form.value;
+      this.authService.createUser(value.email, value.password)
+        .then(() => {
+          this.router.navigate(['/auth/login']);
+        });
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
 
